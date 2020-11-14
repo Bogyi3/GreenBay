00198485f6915b19cd09efa533f5c113939a6743
Boglarka Nagy <n.boglar3@gmail.com>
2020-11-14 18:15:01 +0100

    created db-migration for items table

---
+++ backend/migrations/20201114164303-items.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = (db) => {
+  return db.createTable('items', {
+    id: {
+      type: 'int',
+      primaryKey: true,
+      autoIncrement: true,
+      notNull: true
+    },
+    item_name: {
+      type: 'string',
+      length: 100,
+      notNull: true
+    },
+    item_img: {
+      type: 'string',
+      length: 255,
+      notNull: true
+    },
+    price: {
+      type: 'int',
+      notNull: true
+    },
+    description: {
+      type: 'string',
+      length: 300,
+      notNull: true
+    },
+    seller: {
+      type: 'string',
+      length: 100,
+      notNull: true
+    },
+    buyer: {
+      type: 'string',
+      length: 100,
+    }
+  })
+};
+
+exports.down = (db) => {
+  return db.dropTable('items');
+};
+
+exports._meta = {
+  "version": 1
+};
