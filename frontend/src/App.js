45df2a3036b7eed6d0224109be783facf482ba19
Boglarka Nagy <n.boglar3@gmail.com>
2020-11-14 17:41:08 +0100

    added navbar -mobile view & closing dropdown menu needs to be fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,23 +5,29 @@ import {
   Switch,
   Route,
 } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar';
 
 import LandingPage from './pages/LandingPage/LandingPage';
 import StorePage from './pages/StorePage/StorePage';
 import RegistrationPage from './pages/RegistrationPage/RegistrationPage';
 import LoginPage from './pages/LoginPage/LoginPage';
-import SellItemPage from './pages/SellItemPage/SellItemPage';
+import myItemsPage from './pages/myItemsPage/myItemsPage';
+import ProfilePage from './pages/ProfilePage/ProfilePage';
+import FavoritesPage from './pages/FavoritesPage/FavoritesPage';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <NavBar />
         <Switch>
           <Route exact path="/" component={LandingPage} />
           <Route exact path="/store" component={StorePage} />
           <Route exact path="/register" component={RegistrationPage} />
           <Route exact path="/login" component={LoginPage} />
-          <Route exact path="/sell" component={SellItemPage} />
+          <Route exact path="/myItems" component={myItemsPage} />
+          <Route exact path="/profile" component={ProfilePage} />
+          <Route exact path="/favorites" component={FavoritesPage} />
         </Switch>
       </Router>
     </div>
---
+++ frontend/src/components/NavBar/NavBar.js
@@ -0,0 +1,52 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { logOutAction } from '../../actions/loginActions';
+import './NavBar.css';
+
+function NavBar() {
+  const [open, setOpen] = useState(false);
+  const loginData = useSelector((state) => state.login);
+  const dispatch = useDispatch();
+
+  function logout() {
+    dispatch(logOutAction());
+    setOpen(false);
+  }
+
+  return (
+    <>
+      <div className="navBar">
+        <div className="navBarTitle" onClick={() => setOpen(false)}>
+          <h1>GreenBay</h1>
+        </div>
+        <div className="navBarLogos">
+          {loginData.username && (<Link to="/store" style={{ textDecoration: 'none' }}><i onClick={() => setOpen(false)} className="fas fa-store fa-lg" /></Link>)}
+          <i onClick={() => setOpen(!open)} className="fas fa-user-circle fa-lg" />
+        </div>
+      </div>
+      <div className="navBarUserMenu" style={{ display: open ? 'flex' : 'none' }}>
+        {!loginData.username
+        && (
+        <div className="navBarUserMenuVisitor">
+          <Link to="/login" style={{ textDecoration: 'none' }}><div onClick={() => setOpen(!open)}>Login</div></Link>
+          <Link to="/register" style={{ textDecoration: 'none' }}><div onClick={() => setOpen(!open)}>Sign up</div></Link>
+        </div>
+        )}
+        {loginData.username
+        && (
+        <div className="navBarUserMenuCustomer">
+          <Link to="/profile" style={{ textDecoration: 'none' }}><div onClick={() => setOpen(!open)}>Profile</div></Link>
+          <Link to="/myItems" style={{ textDecoration: 'none' }}><div onClick={() => setOpen(!open)}>My Items</div></Link>
+          <Link to="/favorites" style={{ textDecoration: 'none' }}><div onClick={() => setOpen(!open)}>Favorites</div></Link>
+          <Link to="/" style={{ textDecoration: 'none' }}><div onClick={logout}>Logout</div></Link>
+        </div>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default NavBar;
